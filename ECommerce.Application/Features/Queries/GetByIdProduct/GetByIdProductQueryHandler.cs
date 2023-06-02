628465357f384241abd67d0df64bc876b4d44ebf
mert-celik-1 <mertcelik2399@gmail.com>
2023-06-02 10:34:39 +0300

    cqrs implementation

---
+++ ECommerce.Application/Features/Queries/GetByIdProduct/GetByIdProductQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using ECommerce.Application.Repositories.Product;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Application.Features.Queries.GetByIdProduct
+{
+    internal class GetByIdProductQueryHandler : IRequestHandler<GetByIdProductQueryRequest, GetByIdProductQueryResponse>
+    {
+
+        readonly IProductReadRepository _productReadRepository;
+        public GetByIdProductQueryHandler(IProductReadRepository productReadRepository)
+        {
+            _productReadRepository = productReadRepository;
+        }
+
+        public async Task<GetByIdProductQueryResponse> Handle(GetByIdProductQueryRequest request, CancellationToken cancellationToken)
+        {
+            var product = await _productReadRepository.GetByIdAsync(request.Id, false);
+            return new()
+            {
+                Name = product.Name,
+                Price = product.Price,
+                Stock = product.Stock
+            };
+        }
+    }
+}
